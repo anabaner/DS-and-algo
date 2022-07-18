b908fa1c40ab7423f5d9d25ef2989033fa669a10
anabaner <50626061+anabaner@users.noreply.github.com>
2022-07-18 16:46:08 +0530

    Update MaxProfitonBuySellStock.java

--- MaxProfitonBuySellStock.java
+++ MaxProfitonBuySellStock.java
@@ -4,12 +4,20 @@ public class MaxProfitonBuySellStock {
 
 	static int maxProfit(int prices[], int size)
     {
-        int maxProfit = 0;
-     
-        for (int i = 1; i < size; i++)
-            if (prices[i] > prices[i - 1])
-                maxProfit += prices[i] - prices[i - 1];
-        return maxProfit;
+       
+		 if(prices==null||prices.length<=1)
+		        return 0;
+		 
+		    int min=prices[0]; // min so far
+		    int result=0;
+		 
+		    for(int i=1; i<prices.length; i++){
+		        result = Math.max(result, prices[i]-min);
+		        min = Math.min(min, prices[i]);
+		    }
+		 
+		 return result;
+		 
     }
  
     public static void main(String[] args)
